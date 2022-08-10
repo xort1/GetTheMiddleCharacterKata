3d214a56dc0f10a2fc0e75be64190229765ce5b3
Daniil Gubanov <yranidrop@gmail.com>
2022-08-10 19:01:45 +0300

    Добавьте файлы проекта.

---
+++ 10.08.22/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Kata
+{
+    static void Main(string[] args)
+    {
+        string s = Console.ReadLine();
+        GetMiddle(s);
+    }
+
+    public static string GetMiddle(string s)
+    {
+        char[] arr;
+        arr = s.ToCharArray();
+        if (arr.Length % 2 == 0)
+        {
+            s = Convert.ToString(arr[(arr.Length - 1) / 2]) + Convert.ToString(arr[((arr.Length - 1) / 2) + 1]);
+            return s;
+        }
+        else
+        {
+            s = Convert.ToString(arr[arr.Length / 2]);
+            return s;
+        }
+    }
+}
